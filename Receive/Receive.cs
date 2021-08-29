60641363c4719dd1adfb808780a4384b329359a3
Ethan Lam <hellowah5@gmail.com>
2021-08-29 22:39:14 +0800

    'Initial'

---
+++ Receive/Receive.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receive
+{
+    //Consumer: listening for messages from RabbitMQ
+    class Receive
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //Declare the queue from which we're going to consume
+                    channel.QueueDeclare(queue: "hello",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                    
+                    //Tell the server to deliver consumer the messages from the queue
+                    //Provide a callback since messages are pushed asynchronously
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+
+                    channel.BasicConsume(queue:"hello",
+                                        autoAck: true,
+                                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    //Producer: sending messages to RabbitMQ
+    class Send
+    {
+        public static void Main(string[] args)
+        {
+            // Connect to a RabbitMQ node on the local machine
+            // If you wanted to connect to a node on a different machine, we'd simply sepcify its hostname or IP address.
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //Declare a queue
+                    channel.QueueDeclare(queue: "hello", 
+                                        durable: false, 
+                                        exclusive: false, 
+                                        autoDelete: false, 
+                                        arguments: null);
+
+                    Console.WriteLine(" Input your message, then press [enter]");                    
+                    string message = Console.ReadLine();
+
+                    //string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    //Publish message to the queue
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "hello",
+                                        basicProperties: null,
+                                        body: body);
+                    
+                    Console.WriteLine(" [x] Sent {0}", message);                    
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
